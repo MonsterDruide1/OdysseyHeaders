854eadf83c079cd002afe93ac05506bd89fec457
vakhdmyt06 <vakhdmyt06@gmail.com>
2024-04-20 20:51:35 +0000

    Implement ActorActionKeeper and ActorAnimFunction + AnimPlayer* headers

---
+++ al/Library/LiveActor/ActorInitResourceData.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Project/Action/InitResourceDataAction.h"
+#include "Project/Anim/InitResourceDataAnim.h"
+
+namespace al {
+class Resource;
+
+class ActorInitResourceData {
+public:
+    ActorInitResourceData(Resource*);
+
+    InitResourceDataAction* getDataAction() const { return mResDataAction; }
+
+public:
+    Resource* mResource;
+    InitResourceDataAnim* mResDataAnim = nullptr;
+    InitResourceDataAction* mResDataAction = nullptr;
+};
+}  // namespace al
