4d6b16b86264ddc7d39f95aedfb7f6fef1c4a842
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-05-16 11:15:47 +0000

    add:ApplicationMessageReceiver header

---
+++ al/Library/Application/ApplicationMessageReceiver.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <nn/oe.h>
+
+namespace al {
+class ApplicationMessageReceiver {
+public:
+    ApplicationMessageReceiver();
+
+    nn::oe::OperationMode getOperationMode() const;
+    nn::oe::PerformanceMode getPerformaceMode() const;
+
+    void init();
+    void procMessage(u32 message);
+    void update();
+
+public:
+    bool mIsUpdatedOperationMode = false;
+    bool mIsUpdatedPerformanceMode = false;
+    nn::oe::OperationMode mOperationMode = nn::oe::OperationMode_Handheld;
+    nn::oe::PerformanceMode mPerformanceMode = nn::oe::PerformanceMode_Normal;
+};
+
+static_assert(sizeof(ApplicationMessageReceiver) == 0xc);
+}  // namespace al
