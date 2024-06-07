247b5be833e3f88c971dea8ff11a674b53ece84c
MonsterDruide1 <5958456@gmail.com>
2024-06-07 22:17:50 +0000

    lint: Enforce short names for `sead::`-math

--- game/Util/PlayerCollisionUtil.h
+++ game/Util/PlayerCollisionUtil.h
@@ -2,16 +2,10 @@
 
 #include <basis/seadTypes.h>
 
-namespace al {
-class LiveActor;
-}
-class IUsePlayerCollision;
 class IUsePlayerHeightCheck;
-class PlayerConst;
 
 namespace rs {
 
 f32 getGroundHeight(const IUsePlayerHeightCheck*);
-bool isCollidedGroundRunAngle(const al::LiveActor*, const IUsePlayerCollision*, const PlayerConst*);
 
-}  // namespace rs
+}
