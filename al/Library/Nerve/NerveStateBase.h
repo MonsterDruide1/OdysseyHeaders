e2ebe736161cd20b9524079960b6b8191bf5cec3
GRAnimated <GRAnimated@noreply.github.com>
2024-10-19 07:40:50 +0000

    Enemy: Implement `EnemyCap` and `EnemyState`s

--- al/Library/Nerve/NerveStateBase.h
+++ al/Library/Nerve/NerveStateBase.h
@@ -6,13 +6,14 @@ namespace al {
 class NerveStateBase : public NerveExecutor {
 public:
     NerveStateBase(const char* stateName);
-    virtual ~NerveStateBase();
 
     virtual void init();
     virtual void appear();
     virtual void kill();
     virtual bool update();
     virtual void control();
+
+    void setDead(bool isDead) { mIsDead = isDead; }
 
     bool isDead() const { return mIsDead; }
 
