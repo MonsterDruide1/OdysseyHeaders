e84f1cd9dbd5c927da5fe0238a519509aa6d61d5
MonsterDruide1 <5958456@gmail.com>
2024-10-31 22:24:23 +0000

    Library/Collision: Implement `al::HitInfo` and variants

--- al/Library/Obj/PartsFunction.h
+++ al/Library/Obj/PartsFunction.h
@@ -4,46 +4,17 @@
 
 namespace al {
 class ActorInitInfo;
-class BreakModel;
-class HitSensor;
-class HostSyncTowerCamera;
-class LiveActor;
 class CollisionObj;
-class PartsModel;
-class KeyMoveCameraObj;
+class HitSensor;
+class LiveActor;
+class Resource;
 
 CollisionObj* createCollisionObj(const LiveActor* parent, const ActorInitInfo& info,
                                  const char* collisionFileName, HitSensor* hitSensor,
                                  const char* joinMtxName, const char* suffix);
 CollisionObj* createCollisionObjMtx(const LiveActor* parent, const ActorInitInfo& info,
-                                    const char* collisionFileName, HitSensor* hitSensor,
-                                    const sead::Matrix34f* joinMtx, const char* suffix);
-
-PartsModel* createPartsModel(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                             const sead::Matrix34f*);
-PartsModel* createPartsModelFile(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                 const char*);
-PartsModel* createPartsModelFileSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                       const char*, const char*);
-PartsModel* createSimplePartsModel(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                   const char*);
-PartsModel* createSimplePartsModelSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                         const char*, const char*);
-PartsModel* createPartsModelSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                   const char*, const sead::Matrix34f*);
-PartsModel* createPartsModelJoint(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                  const char*);
-PartsModel* createPartsModelSuffixJoint(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                        const char*, const char*);
-void appearBreakModelRandomRotateY(LiveActor*);
-bool updateSyncHostVisible(bool*, LiveActor*, const LiveActor*, bool);
+                                    Resource* res, const char* collisionFileName,
+                                    HitSensor* hitSensor, const sead::Matrix34f* joinMtx,
+                                    const char* suffix);
 bool isTraceModelRandomRotate(const LiveActor* actor);
-BreakModel* createBreakModel(const LiveActor*, const ActorInitInfo&, const char*, const char*,
-                             const char*, const sead::Matrix34f*, const char*);
-HostSyncTowerCamera* createHostSyncTowerCamera(LiveActor*, const ActorInitInfo&);
-void activateHostSyncTowerCamera(HostSyncTowerCamera*);
-void deactivateHostSyncTowerCamera(HostSyncTowerCamera*);
-void updateHostSyncTowerCamera(HostSyncTowerCamera*);
-KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObj(const ActorInitInfo&, const char*);
-KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObjWithInterpole(const ActorInitInfo&, const char*);
 }  // namespace al
