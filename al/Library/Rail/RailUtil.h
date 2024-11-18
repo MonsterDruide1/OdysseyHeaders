189521bd3b5b431b91ca7f9e1ca24639884ff63f
MonsterDruide1 <5958456@gmail.com>
2024-11-18 12:03:03 +0000

    wip

--- al/Library/Rail/RailUtil.h
+++ al/Library/Rail/RailUtil.h
@@ -6,7 +6,6 @@ namespace al {
 class IUseRail;
 class LiveActor;
 
-void setSyncRailToNearestPos(LiveActor* actor);
 void setRailPosToStart(IUseRail* railHolder);
 void moveSyncRail(LiveActor* actor, f32 speed);
 void moveSyncRailLoop(LiveActor* actor, f32 speed);
