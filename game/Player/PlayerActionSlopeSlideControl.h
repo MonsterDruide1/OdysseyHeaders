9adcd3cb34a26e612bf8016b6dc69bece31e8f07
MonsterDruide1 <MonsterDruide1@noreply.github.com>
2025-02-02 16:35:26 +0000

    Player: Implement `PlayerActionSlopeSlideControl`

--- game/Player/PlayerActionSlopeSlideControl.h
+++ game/Player/PlayerActionSlopeSlideControl.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <basis/seadTypes.h>
+#include <math/seadVector.h>
 
 namespace al {
 class LiveActor;
@@ -11,14 +11,21 @@ class IUsePlayerCollision;
 
 class PlayerActionSlopeSlideControl {
 public:
-    PlayerActionSlopeSlideControl(al::LiveActor*, const PlayerConst*, const PlayerInput*,
-                                  const IUsePlayerCollision*);
+    PlayerActionSlopeSlideControl(al::LiveActor* player, const PlayerConst* pConst,
+                                  const PlayerInput* input, const IUsePlayerCollision* collider);
     void setup();
     void setupCutSlideOppositeDir();
-    f32 update(f32, f32, f32, f32, f32, f32, f32, f32, f32, f32, bool);
+    f32 update(f32 accel, f32 brake, f32 against, f32 anglePowerMax, f32 maxSpeed, f32 sideAccel,
+               f32 sideBrake, f32 sideMaxSpeed, f32 turnDegree, f32 gravity, bool isRolling);
 
 public:
-    void* filler[0x48 / 8];
+    al::LiveActor* mPlayer;
+    const PlayerConst* mConst;
+    const PlayerInput* mInput;
+    const IUsePlayerCollision* mCollision;
+    sead::Vector3f mDirSlide = sead::Vector3f::zero;
+    sead::Vector3f mGroundNormal = sead::Vector3f::zero;
+    sead::Vector3f mHorizontalVelocity = sead::Vector3f::zero;
 };
 
 static_assert(sizeof(PlayerActionSlopeSlideControl) == 0x48);
