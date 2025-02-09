b24f5619e78b070884d52bcc425fb91d24902501
MonsterDruide1 <5958456@gmail.com>
2025-02-09 23:13:59 +0000

    keep sorting in file list

--- game/Player/PlayerActionSlopeSlideControl.h
+++ game/Player/PlayerActionSlopeSlideControl.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <basis/seadTypes.h>
-#include <math/seadVector.h>
 
 namespace al {
 class LiveActor;
@@ -18,20 +17,8 @@ public:
     void setupCutSlideOppositeDir();
     f32 update(f32, f32, f32, f32, f32, f32, f32, f32, f32, f32, bool);
 
-    const sead::Vector3f& getDirSlide() const { return mDirSlide; }
-
-    const sead::Vector3f& getGroundNormal() const { return mGroundNormal; }
-
-    const sead::Vector3f& getHorizontalVelocity() const { return mHorizontalVelocity; }
-
 public:
-    al::LiveActor* mPlayer;
-    const PlayerConst* mConst;
-    const PlayerInput* mInput;
-    const IUsePlayerCollision* mCollision;
-    sead::Vector3f mDirSlide = {0.0f, 0.0f, 0.0f};
-    sead::Vector3f mGroundNormal = {0.0f, 0.0f, 0.0f};
-    sead::Vector3f mHorizontalVelocity = {0.0f, 0.0f, 0.0f};
+    void* filler[0x48 / 8];
 };
 
 static_assert(sizeof(PlayerActionSlopeSlideControl) == 0x48);
