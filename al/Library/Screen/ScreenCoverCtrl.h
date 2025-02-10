c830455d040e391ec44dc84969c5614cc525b8e8
LynxDev2 <LynxDev2@noreply.github.com>
2025-02-10 15:53:27 +0000

    Library/LiveActor: Implement `ActorSceneFunction`

---
+++ al/Library/Screen/ScreenCoverCtrl.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace al {
+
+class ScreenCoverCtrl {
+public:
+    ScreenCoverCtrl();
+
+    void update();
+    void reqeustCaptureScreenCover(s32 coverFrames);
+
+public:
+    s32 mCoverFrames = -1;
+    bool mIsCover = false;
+};
+
+static_assert(sizeof(ScreenCoverCtrl) == 0x8);
+}  // namespace al
