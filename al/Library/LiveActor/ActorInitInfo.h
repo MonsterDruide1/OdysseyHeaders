7366cbb5de31ef76f4e260517be5944674e4233d
MonsterDruide1 <5958456@gmail.com>
2025-02-25 00:49:05 +0000

    progress

--- al/Library/LiveActor/ActorInitInfo.h
+++ al/Library/LiveActor/ActorInitInfo.h
@@ -63,6 +63,8 @@ public:
 
     ExecuteDirector* getExecuteDirector() const { return mExecuteDirector; }
 
+    ActorResourceHolder* getActorResourceHolder() const { return mActorResourceHolder; }
+
 public:
     LiveActorGroup* mKitDrawingGroup = nullptr;
     const PlacementInfo* mPlacementInfo = nullptr;
