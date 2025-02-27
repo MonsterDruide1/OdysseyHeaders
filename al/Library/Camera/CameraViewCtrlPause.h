50c20a8663c626fb8652e8bcaf75b29a4e6dd80c
Moddimation <Moddimation@noreply.github.com>
2025-02-27 19:09:51 +0000

    Library/Camera: Implement CameraPoser & -SceneInfo + Headers

---
+++ al/Library/Camera/CameraViewCtrlPause.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace al {
+
+class PauseCameraCtrl {
+public:
+    PauseCameraCtrl(f32 v);
+
+public:
+    bool mIsCameraPause = false;
+    f32 _4;
+};
+
+static_assert(sizeof(PauseCameraCtrl) == 0x8);
+
+}  // namespace al
