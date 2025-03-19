9380840fa5f968e6797fffdb1c9d5ecd2f3ef937
GRAnimated <GRAnimated@noreply.github.com>
2025-03-19 00:13:10 +0000

    Util: Implement `ItemGenerator` and `ItemUtil`

--- game/Scene/HintPhotoLayoutHolder.h
+++ game/Scene/HintPhotoLayoutHolder.h
@@ -11,7 +11,7 @@ class DecideIconLayout;
 class HintPhotoLayoutHolder : public al::ISceneObj {
 public:
     HintPhotoLayoutHolder();
-    void init(const al::LayoutInitInfo& initInfo);
+    void init(const al::LayoutInitInfo& info);
     bool isInit() const;
     const al::LayoutActor* getPhotoLayout() const;
     DecideIconLayout* getDecideIcon() const;
