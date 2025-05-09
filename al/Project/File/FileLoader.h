0e1feabc7275faa549c756608724c0dcaf425e8b
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-05-09 20:48:50 +0000

    Library/File: Implement `FileUtil` (#585)

--- al/Project/File/FileLoader.h
+++ al/Project/File/FileLoader.h
@@ -4,15 +4,15 @@
 #include <thread/seadCriticalSection.h>
 
 namespace sead {
+class ArchiveRes;
 class FileDevice;
-class Resource;
 }  // namespace sead
 
 namespace al {
-class IAudioResourceLoader;
 class ByamlIter;
-class SoundItemHolder;
 class FileEntryBase;
+class IAudioResourceLoader;
+class SoundItemHolder;
 }  // namespace al
 
 namespace al {
@@ -33,14 +33,14 @@ public:
     bool isExistArchive(const sead::SafeString& path, sead::FileDevice* device) const;
     bool isExistDirectory(const sead::SafeString& path, sead::FileDevice* device) const;
     u32 getFileSize(const sead::SafeString& path, sead::FileDevice* device) const;
-    u8* loadfile(const sead::SafeString&, s32, sead::FileDevice*);
+    u8* loadFile(const sead::SafeString&, s32, sead::FileDevice*);
     bool tryLoadFileToBuffer(const sead::SafeString& path, u8* buffer, u32, s32,
                              sead::FileDevice* device);
-    sead::Resource* loadArchive(const sead::SafeString& path, sead::FileDevice* device);
-    sead::Resource* loadArchiveLocal(const sead::SafeString& path, const char* suffix,
-                                     sead::FileDevice* device);
-    sead::Resource* loadArchiveWithExt(const sead::SafeString& path, const char* suffix,
+    sead::ArchiveRes* loadArchive(const sead::SafeString& path, sead::FileDevice* device);
+    sead::ArchiveRes* loadArchiveLocal(const sead::SafeString& path, const char* suffix,
                                        sead::FileDevice* device);
+    sead::ArchiveRes* loadArchiveWithExt(const sead::SafeString& path, const char* suffix,
+                                         sead::FileDevice* device);
     bool tryRequestLoadArchive(const sead::SafeString& path, sead::Heap* heap,
                                sead::FileDevice* device);
     FileEntryBase* requestLoadArchive(const sead::SafeString& path, sead::Heap* heap,
@@ -62,6 +62,7 @@ public:
     sead::FileDevice* mFileDevice;
     sead::CriticalSection mCriticalSection;
 };
-}  // namespace al
 
-static_assert(sizeof(al::FileLoader) == 0x68, "al::FileLoader size");
+static_assert(sizeof(FileLoader) == 0x68, "FileLoader size");
+
+}  // namespace al
