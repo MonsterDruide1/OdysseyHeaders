a59772d3d5910cbdc41a2b92592b6a66a4daccc8
LynxDev2 <LynxDev2@noreply.github.com>
2025-06-07 19:11:01 +0000

    src: Fix param name mismatches between declarations and definitions

--- game/Enemy/EnemyStateWander.h
+++ game/Enemy/EnemyStateWander.h
@@ -15,7 +15,7 @@ public:
     bool isWait() const;
     bool isWalk() const;
     bool isFall() const;
-    void changeWalkAnim(const char*);
+    void changeWalkAnim(const char* animName);
 
 public:
     const char* mStateName;
