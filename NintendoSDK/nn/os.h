67df287048a33718d4861b121440f0ac554b9fb4
MonsterDruide1 <5958456@gmail.com>
2025-07-15 21:14:15 +0000

    update sead

--- NintendoSDK/nn/os.h
+++ NintendoSDK/nn/os.h
@@ -39,7 +39,11 @@ struct InterProcessEventType {
 };
 }  // namespace detail
 
-typedef u64 Tick;
+struct Tick {
+    Tick(u64 val) : value(val) {}
+
+    u64 value;
+};
 
 struct LightEventType {
     std::aligned_storage_t<0xc, 4> storage;
