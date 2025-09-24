10b8e3c83e176252ca4670c62f186a1cfb0a2fc4
MonsterDruide1 <MonsterDruide1@noreply.github.com>
2025-09-24 22:10:34 +0000

    Library/LiveActor: Implement `ActorInitFunction`

--- al/Library/Collision/CollisionParts.h
+++ al/Library/Collision/CollisionParts.h
@@ -11,9 +11,13 @@ class HitSensor;
 
 class CollisionParts {
 public:
+    CollisionParts(void* kcl, const void* byml);
+
     const LiveActor* getConnectedHost() const;
     void calcForceMovePower(sead::Vector3f*, const sead::Vector3f&) const;
     void calcForceRotatePower(sead::Quatf*) const;
+    void initParts(const sead::Matrix34f&);
+    void invalidateBySystem();
 
     const sead::Matrix34f& getBaseMtx() const { return mBaseMtx; }
 
@@ -21,23 +25,31 @@ public:
 
     const sead::Matrix34f& getPrevBaseMtx() const { return mPrevBaseMtx; }
 
-    const sead::Matrix34f& getPrevBaseInvMtx() const { return mPrevBaseInvMtx; }
-
     KCollisionServer* getKCollisionServer() const { return mKCollisionServer; }
 
     s32 get_15c() const { return _15c; }
-
-    bool isValidCollision() const { return mIsValidatedByUser && mIsValidatedBySystem; }
 
     bool isMoving() const { return mIsMoving; }
 
     const HitSensor* getConnectedSensor() const { return mConnectedSensor; }
 
+    void set_16e(bool val) { _16e = val; }
+
+    void setSpecialPurpose(const char* specialPurpose) { mSpecialPurpose = specialPurpose; }
+
+    void setOptionalPurpose(const char* optionalPurpose) { mOptionalPurpose = optionalPurpose; }
+
+    void setPriority(s32 priority) { mPriority = priority; }
+
+    void setConnectedSensor(HitSensor* sensor) { mConnectedSensor = sensor; }
+
+    void setJointMtx(const sead::Matrix34f* jointMtx) { mJointMtx = jointMtx; }
+
 public:
     void* unk[2];
     CollisionParts* _10;  // self-reference
     sead::TList<CollisionParts*>* mPartsList;
-    sead::Matrix34f* mJointMtx;
+    const sead::Matrix34f* mJointMtx;
     sead::Matrix34f mSyncMtx;
     sead::Matrix34f mBaseMtx;
     sead::Matrix34f mBaseInvMtx;
