8ad9149e3c9f63f0b5d43888c62b0e085324cad7
guymakinggames <guymakinggames@noreply.github.com>
2026-05-04 17:20:09 +0000

    Demo: Implement `DemoActorPeach`

--- al/Library/Demo/DemoActor.h
+++ al/Library/Demo/DemoActor.h
@@ -1,33 +1,39 @@
 #pragma once
 
-#include <math/seadQuat.h>
-#include <math/seadVector.h>
+#include <basis/seadTypes.h>
+#include <math/seadMatrix.h>
 
 #include "Library/LiveActor/LiveActor.h"
 
 namespace al {
+struct ActorInitInfo;
+class DemoActionList;
+class DemoDynamicsResetInfoHolder;
+class JointSpringControllerHolder;
+
 class DemoActor : public LiveActor {
 public:
     DemoActor(const char* name);
 
+    virtual void initDemoActor(const ActorInitInfo& initInfo, const ActorInitInfo& createInfo,
+                               const sead::Matrix34f* baseMtx, bool isSerial);
+    void initCommon(const ActorInitInfo& initInfo, const ActorInitInfo& createInfo,
+                    const sead::Matrix34f* baseMtx, bool isSerial);
+    virtual void initDemoActorSerial(const ActorInitInfo& initInfo, const ActorInitInfo& createInfo,
+                                     const sead::Matrix34f* baseMtx);
+
     void init(const ActorInitInfo& info) override;
+    virtual void initAfterCreateFromFactory(const ActorInitInfo& initInfo,
+                                            const ActorInitInfo& createInfo,
+                                            const sead::Matrix34f* baseMtx, bool isSerial);
     void control() override;
 
-    virtual void initDemoActor(const ActorInitInfo&, const ActorInitInfo&, const sead::Matrix34f*,
-                               bool);
-    virtual void initDemoActorSerial(const ActorInitInfo&, const ActorInitInfo&,
-                                     const sead::Matrix34f*);
-    virtual void initAfterCreateFromFactory(const ActorInitInfo&, const ActorInitInfo&,
-                                            const sead::Matrix34f*, bool);
-    virtual void startAction(s32);
-
-    virtual void resetDynamics();
-
-    void initCommon(const ActorInitInfo&, const ActorInitInfo&, const sead::Matrix34f*, bool);
-    void startDemo(const sead::Matrix34f&);
-    bool isExistAction(s32) const;
-    const char* getDemoActionName(s32) const;
+    void startDemo(const sead::Matrix34f& baseMtx);
+    virtual void startAction(s32 actionIndex);
+    bool isExistAction(s32 actionIndex) const;
+    const char* getDemoActionName(s32 actionIndex) const;
     void startActionByName(const char* actionName);
+    virtual void resetDynamics();
     void hideModelBySwitch();
     void showModelBySwitch();
     void endDemo();
@@ -35,8 +41,18 @@ public:
     void exeAction();
 
 public:
-    u8 _108[0x60];
-    bool _168 = false;
+    DemoActionList* mDemoActionList = nullptr;
+    sead::Matrix34f mPlacementBaseMtx = sead::Matrix34f::ident;
+    s32 mActionDelay = 0;
+    s32 mActionIndex = 0;
+    s32 mShowModelStartFrame = -1;
+    s32 mShowModelEndFrame = -1;
+    bool mIsSerial = false;
+    bool mIsDemoSceneActor = false;
+    bool mIsValidDynamics = true;
+    JointSpringControllerHolder* mJointSpringControllerHolder = nullptr;
+    DemoDynamicsResetInfoHolder* mDemoDynamicsResetInfoHolder = nullptr;
+    bool mIsFollowCamera = false;
 };
 
 static_assert(sizeof(DemoActor) == 0x170);
