baa76fb5075c9b855162e849a31e6a6460ce71f8
guymakinggames <guymakinggames@noreply.github.com>
2026-08-05 01:11:05 +0000

    MapObj: Implement `RiseMapPartsOld`

--- game/Amiibo/HelpAmiiboDirector.h
+++ game/Amiibo/HelpAmiiboDirector.h
@@ -7,8 +7,6 @@
 #include "Library/Nerve/IUseNerve.h"
 #include "Library/Scene/ISceneObj.h"
 
-#include "Scene/SceneObjFactory.h"
-
 namespace al {
 struct NfpInfo;
 
@@ -32,8 +30,6 @@ class HelpAmiiboDirector : public al::IUseHioNode,
                            public al::IUseAudioKeeper,
                            public al::IUseNerve {
 public:
-    static constexpr s32 sSceneObjId = SceneObjID_HelpAmiiboDirector;
-
     HelpAmiiboDirector();
     void init(ProjectNfpDirector* projectNfpDirector, const al::PlayerHolder* playerHolder,
               al::AudioDirector* audioDirector, const al::LayoutInitInfo& initInfo);
@@ -51,16 +47,14 @@ public:
     void killCoinCollectEffect();
     bool tryExecute(const al::NfpInfo* nfpInfo);
 
+    const char* getSceneObjName() const override;
+    al::AudioKeeper* getAudioKeeper() const override;
+    al::NerveKeeper* getNerveKeeper() const override;
+
     void exeWait();
     void exeCountHold();
     void exeActive();
     void exeIconClose();
-
-    const char* getSceneObjName() const override { return "アイテム出現用amiiboディレクター"; }
-
-    al::AudioKeeper* getAudioKeeper() const override { return mAudioKeeper; }
-
-    al::NerveKeeper* getNerveKeeper() const override { return mNerveKeeper; }
 
 public:
     ProjectNfpDirector* mProjectNfpDirector = nullptr;
@@ -69,9 +63,9 @@ public:
     sead::PtrArray<HelpAmiiboExecutor> mTouchEntries;
 
     al::NerveKeeper* mNerveKeeper = nullptr;
-    al::SimpleLayoutAppearWaitEnd* mAmiiboIcon = nullptr;
-    al::LayoutActor* mAmmiiboIconParts = nullptr;
-    bool mIsEnableAmiibo = true;
+    al::SimpleLayoutAppearWaitEnd* mSimpleLayout = nullptr;
+    al::LayoutActor* mLayoutActor = nullptr;
+    bool _58 = true;
     HelpAmiiboCoinCollect* mCoinCollect = nullptr;
 };
 
