fa36da9ecaab599c31ff01525e209d055a2fdab7
german77 <german77@noreply.github.com>
2026-09-13 20:34:28 +0000

    all: Enforce weak / global functions

--- sead/devenv/seadFontMgr.h
+++ sead/devenv/seadFontMgr.h
@@ -34,7 +34,7 @@ public:
 class FontBase
 {
 public:
-    virtual ~FontBase();
+    virtual ~FontBase() = default;
 
     virtual float getHeight() const = 0;
     virtual float getWidth() const = 0;
